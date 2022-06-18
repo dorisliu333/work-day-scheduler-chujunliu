65259778070034f0e0d9e2e3f233a4885ac4a553
DorisLiu <dorisliu113@gmail.com>
2022-06-18 18:48:14 +1000

    add coments to js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+//get dom element by using jquery
 var currentDay = $("#currentDay");
 var timeBlockEl = $(".time-block");
 var descriptionEl = $(".description");
 
+//compare with current time
 function checkTime() {
     var today = moment().format("h:mma, dddd, MMMM Do");
+    //display the current time
     currentDay.text(today);
     timeBlockEl.map(item => {
         const timeConvert = moment(timeBlockEl[item].children[0].innerText, "ha").format('HH:mm');
@@ -17,14 +20,14 @@ function checkTime() {
         }
     })
 }
-
+//show description
 function printDescription() {
     descriptionEl.css("display","block")
     setInterval(function () {
         descriptionEl.css("display","none")
     }, 3000);
 }
-
+//get and store input value
 function getInputValue(event) {
     var btnClicked = $(event.target);
     const currentHour = btnClicked.siblings("div").data("hour");
@@ -32,15 +35,16 @@ function getInputValue(event) {
     localStorage.setItem(currentHour, currentInput);
     printDescription();
 }
-
+//print the vaule which store in the local storage
 function printEvents() {
     timeBlockEl.map(item => {
         var event = localStorage.getItem(timeBlockEl[item].innerText);
         timeBlockEl[item].children[1].innerText = event;
     })
 }
-
-
+//keep updating the time
 setInterval(checkTime, 1000)
+//add event to save button
 timeBlockEl.on("click", ".saveBtn", getInputValue)
+//call function to print the events when refresh
 printEvents();
